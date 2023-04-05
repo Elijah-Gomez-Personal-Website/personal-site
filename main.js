130d88aabb6c65978180fdb2e7ca097743bdbaa7
Elijah Gomez <elilee@live.com>
2023-04-04 22:54:31 -0400

    Did a lot of work on the overall layout and got a new font from google fonts

---
+++ main.js
@@ -0,0 +1,12 @@
+const externalLink = document.querySelector('#external_link');
+
+externalLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    const link = e.target.href;
+
+    const navigateAway = confirm("Are you sure you want to navigate away from this website?");
+
+    if (navigateAway) {
+        window.location = link;
+    }
+});
