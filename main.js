2eb71af724c1737477b7d938dff9144d1dfdfeaa
Elijah Gomez <elilee@live.com>
2023-04-06 22:41:05 -0400

    Filled out a good amount of content for the index.html

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const externalLink = document.querySelector('#external_link');
 
 externalLink.addEventListener('click', function(e) {
     e.preventDefault();
-    const link = e.target.href;
+
+    // console.log(e.target)
+    // console.log(externalLink);
+    // const link = e.target.href;
+    
+    const link = externalLink.href;
 
     const navigateAway = confirm("Are you sure you want to navigate away from this website?");
 
